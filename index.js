b72f548b36fbbc4f40d489a7dbf813ac2fb77ea3
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-08-29 00:50:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -9,9 +9,76 @@
  for you to use if you need it!
  */
 
+ function createEmployeeRecords(array) {
+    return array.map(createEmployeeRecord);
+  }
+function createEmployeeRecord(array) {
+  return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+}
+
+
+function createTimeInEvent(dateStamp) {
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(dateStamp.split(" ")[1], 10),
+    date: dateStamp.split(" ")[0],
+  });
+  return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(dateStamp.split(" ")[1], 10),
+    date: dateStamp.split(" ")[0],
+  });
+
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  let employeeInRecordsMatching = this.timeInEvents.find(function (event) {
+    return event.date === date;
+  });
+
+  let employeeOutRecordsMatching = this.timeOutEvents.find(function (event) {
+    return event.date === date;
+  });
+
+  return (
+    (employeeOutRecordsMatching.hour - employeeInRecordsMatching.hour) / 100
+  );
+}
+
+function wagesEarnedOnDate(date) {
+  let hoursWorked = hoursWorkedOnDate.call(this, date);
+  let wages = hoursWorked * this.payPerHour;
+  return parseFloat(wages.toString());
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find((employee) => {
+    return employee.firstName === firstName;
+  });
+}
+
+function calculatePayroll(arrayOfEmployees) {
+  return arrayOfEmployees.reduce((acc, rec) => {
+    return acc + allWagesFor.call(rec);
+  }, 0);
+}
+
+
 const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
+    const eligibleDates = this.timeInEvents.map(function (event) {
+        return event.date
     })
 
     const payable = eligibleDates.reduce(function (memo, d) {
